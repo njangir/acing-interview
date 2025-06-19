78248664bcb668b11e00bdb12b393df5908408df
Naresh Jangir (NjA) <njak46.jangir@gmail.com>
2025-06-19 05:01:38 +0000

    can you check the overall codebase for one last time before we handover

--- src/app/(main)/book/page.tsx
+++
@@ -1,4 +0,0 @@
-
-// This file is intentionally left blank as it's being removed.
-// The functionality of listing services is now handled by /services page.
-// Specific booking flows like /book/[serviceId]/slots will still work.
