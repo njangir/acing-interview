c89104eb476d8a3a18d8136bb3a3b7d8274e2d88
Naresh Jangir (NjA) <njak46.jangir@gmail.com>
2025-07-29 09:28:02 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,35 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getFunctions } from "firebase/functions";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  projectId: "armed-forces-interview-ace",
+  appId: "1:742450909840:web:bc667d403885a3c75c7e15",
+  storageBucket: "armed-forces-interview-ace.firebasestorage.app",
+  apiKey: "AIzaSyCJy25IhMVNfNvAcIfr05ccgohGjp9zV0w",
+  authDomain: "armed-forces-interview-ace.firebaseapp.com",
+  messagingSenderId: "742450909840",
+};
+
+
+// Initialize Firebase
+// To avoid re-initializing on hot reloads, we check if an app is already initialized.
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const functions = getFunctions(app);
+
+
+export { app, auth, db, storage, functions };
