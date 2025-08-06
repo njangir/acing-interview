5c279557c068da4c96eac2763938a658c780b81c
Naresh Jangir (NjA) <njak46.jangir@gmail.com>
2025-08-06 16:40:27 +0000

    just a visual correction required, do not modify any other part of the a

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,7 +10,7 @@ export default function MainLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container">{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
